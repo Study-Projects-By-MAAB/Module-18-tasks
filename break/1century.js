1a065c12bd8d3524962ab26e866d7b175b912e5a
maabfw <maabfw@proton.me>
2024-01-30 13:23:16 +0600

    on break task 1 'for loop' added and task 2 init.

--- break/1century.js
+++ break/1century.js
@@ -1,7 +1,19 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
 let num = 1;
 while (num <= 200) {
     if (num <= 100) {
         console.log(num);
     }
     num++;
+}
+
+for (num = 1; num <= 200; num++) {
+    if (num <= 100) {
+        console.log(num);
+    }
 }
